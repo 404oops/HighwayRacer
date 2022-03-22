f4306aae3f44653e0f3c432ce11c999200c2ad74
404oops <404oops@404PSC128>
2022-03-22 22:55:19 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,397 @@
+﻿namespace HWR
+{
+    partial class HWR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HWR));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.carlabel = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel6 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox5.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gas";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(847, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(204, 69);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Brzinometar";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 17);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(192, 44);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 178);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 35);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Kocnica";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(22, 194);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "RUCNA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.carlabel);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 240);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(773, 174);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Put";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(187, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(381, 73);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Game Over!";
+            this.label5.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cascadia Code SemiBold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(191, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 75);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "  ___\r\n    _-_-  _/\\______\\\\__\r\n _-_-__  / ,-. -|-  ,-.`-.\r\n    _-_- `( o )----( " +
+    "o )-\'\r\n           `-\'      `-\'\r\n";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Code SemiBold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(405, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 75);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "  ___\r\n    _-_-  _/\\______\\\\__\r\n _-_-__  / ,-. -|-  ,-.`-.\r\n    _-_- `( o )----( " +
+    "o )-\'\r\n           `-\'      `-\'\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code SemiBold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(605, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 75);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "  ___\r\n    _-_-  _/\\______\\\\__\r\n _-_-__  / ,-. -|-  ,-.`-.\r\n    _-_- `( o )----( " +
+    "o )-\'\r\n           `-\'      `-\'\r\n";
+            // 
+            // carlabel
+            // 
+            this.carlabel.AutoSize = true;
+            this.carlabel.Font = new System.Drawing.Font("Cascadia Code SemiBold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carlabel.ForeColor = System.Drawing.Color.Red;
+            this.carlabel.Location = new System.Drawing.Point(10, 98);
+            this.carlabel.Name = "carlabel";
+            this.carlabel.Size = new System.Drawing.Size(163, 75);
+            this.carlabel.TabIndex = 2;
+            this.carlabel.Text = "  ___\r\n    _-_-  _/\\______\\\\__\r\n _-_-__  / ,-. -|-  ,-.`-.\r\n    _-_- `( o )----( " +
+    "o )-\'\r\n           `-\'      `-\'\r\n";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.groupBox6);
+            this.groupBox3.Controls.Add(this.groupBox5);
+            this.groupBox3.Controls.Add(this.groupBox1);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(431, 222);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Auto";
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.trackBar1);
+            this.groupBox6.Location = new System.Drawing.Point(6, 94);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(204, 78);
+            this.groupBox6.TabIndex = 9;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Volan";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(6, 19);
+            this.trackBar1.Maximum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(192, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.checkBox3);
+            this.groupBox5.Controls.Add(this.checkBox2);
+            this.groupBox5.Controls.Add(this.checkBox1);
+            this.groupBox5.Location = new System.Drawing.Point(244, 17);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(181, 92);
+            this.groupBox5.TabIndex = 8;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Kontrolna Tabla";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 66);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(76, 17);
+            this.checkBox3.TabIndex = 8;
+            this.checkBox3.Text = "Tempomat";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 19);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(49, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Kljuc";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 42);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Start/Stop";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel5,
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel4,
+            this.toolStripStatusLabel6});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel5
+            // 
+            this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            this.toolStripStatusLabel5.Size = new System.Drawing.Size(71, 17);
+            this.toolStripStatusLabel5.Text = "Kljuc: Nema";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(78, 17);
+            this.toolStripStatusLabel1.Text = "Auto: Ugasen";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(105, 17);
+            this.toolStripStatusLabel2.Text = "Strana Puta: Desna";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(112, 17);
+            this.toolStripStatusLabel3.Text = "Rucna: Nije Stisnuta";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(108, 17);
+            this.toolStripStatusLabel4.Text = "Interval Tajmera: 10";
+            // 
+            // toolStripStatusLabel6
+            // 
+            this.toolStripStatusLabel6.Name = "toolStripStatusLabel6";
+            this.toolStripStatusLabel6.Size = new System.Drawing.Size(116, 17);
+            this.toolStripStatusLabel6.Text = "Tempomat: Iskljucen";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // HWR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "HWR";
+            this.Text = "AutoSim";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label carlabel;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace HWR
+{
+    public partial class HWR : Form
+    {
+        Random r = new Random(); int x;
+        public HWR()
+        {
+            InitializeComponent();
+            x = label1.Left;
+            timer1.Interval = 160;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                if (timer1.Interval > 20)
+                    timer1.Interval -= 20;
+                else
+                    timer1.Interval = 1;
+                if (timer1.Enabled != true)
+                    timer1.Start();
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Interval += 10;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                if (x == label1.Left)
+                    label1.Left += 50;
+                else if (label1.Left - 50 == x)
+                    label1.Left -= 50;
+                if (!checkBox3.Checked)
+                    timer1.Interval++;
+                groupBox1.Text = "Brzina je " + Math.Abs(timer1.Interval - 200);
+                int v = Math.Abs(timer1.Interval - 200);
+                if (v >= 135 && v <= 200)
+                    progressBar1.SetState(2);
+                else if (v >= 100 && v <= 135)
+                    progressBar1.SetState(3);
+                else
+                    progressBar1.SetState(1);
+                progressBar1.Value = Math.Abs(timer1.Interval - 200) / 2;
+                if (timer1.Interval >= 200)
+                    timer1.Stop();
+                toolStripStatusLabel4.Text = "Interval Tajmera: " + timer1.Interval;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 200;
+            toolStripStatusLabel3.Text = "Rucna: Stisnuta";
+        }
+        private void TurnCar(string direction)
+        {
+            switch (direction) {
+                case "left":
+                    carlabel.Top = 10;
+                    toolStripStatusLabel2.Text = "Strana Puta: Leva";
+                    break;
+                case "right":
+                    carlabel.Top = 96;
+                    toolStripStatusLabel2.Text = "Strana Puta: Desna";
+                    break;
+            }
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            switch (trackBar1.Value)
+            {
+                case 0:
+                    TurnCar("left");
+                    break;
+                case 1:
+                    TurnCar("right");
+                    break;
+            }
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox2.Checked)
+                checkBox1.Checked = false;
+            if (checkBox1.Checked)
+            {
+                toolStripStatusLabel1.Text = "Auto: Upaljen";
+                timer2.Start();
+            }  
+            else
+            {
+                toolStripStatusLabel1.Text = "Auto: Ugasen";
+                timer2.Stop();
+            }
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+                toolStripStatusLabel6.Text = "Tempomat: Upaljen";
+            else
+                toolStripStatusLabel6.Text = "Tempomat: Ugasen";
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+                toolStripStatusLabel5.Text = "Kljuc: Ubacen";
+            else
+                toolStripStatusLabel5.Text = "Kljuc: Nema";
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label2.Left -= 10;
+            label3.Left -= 10;
+            label4.Left -= 10;
+            if (timer1.Interval <= 190)
+            {
+                if (label2.Left <= 10)
+                {
+                    label2.Left = 807;
+                    if (r.Next(2) == 1)
+                        label2.Top = 10;
+                    else
+                        label2.Top = 96;
+                }
+                if (label3.Left <= 10)
+                {
+                    label3.Left = 807;
+                    if (r.Next(2) == 1)
+                        label3.Top = 10;
+                    else
+                        label3.Top = 96;
+                }
+                if (label4.Left <= 10)
+                {
+                    label4.Left = 807;
+                    if (r.Next(2) == 1)
+                        label4.Top = 10;
+                    else
+                        label4.Top = 96;
+                }
+            }
+
+
+            if (carlabel.Bounds.IntersectsWith(label2.Bounds) || 
+                carlabel.Bounds.IntersectsWith(label3.Bounds) ||
+                carlabel.Bounds.IntersectsWith(label4.Bounds))
+                gameOver();
+        }
+        private void gameOver()
+        {
+            checkBox1.Checked = checkBox2.Checked = checkBox3.Checked = false;
+            timer1.Stop();
+            timer2.Stop();
+            carlabel.Visible = label2.Visible = label3.Visible = label4.Visible = false;
+            label5.Visible = true;
+        }
+    }
+    public static class ModifyProgressBarColor
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr w, IntPtr l);
+        public static void SetState(this ProgressBar pBar, int state)
+        {
+            SendMessage(pBar.Handle, 1040, (IntPtr)state, IntPtr.Zero);
+        }
+    }
+}
